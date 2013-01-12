06cf7573bb5bdfacb8c6501f8698305cd28ee2b3
Nitin Hayaran <nitinhayaran@gmail.com>
2013-01-12 23:11:09 +0530

    Fixed post title tag, as I am using SEO Plugin

--- templates/head.php
+++ templates/head.php
@@ -6,7 +6,7 @@
 <head>
   <meta charset="utf-8">
   <link rel="shortcut icon" type="image/x-icon" href="<?php echo get_stylesheet_directory_uri(); ?>/assets/favicon.ico">
-  <title><?php wp_title('|', true, 'right'); bloginfo('name'); ?></title>
+  <title><?php wp_title('|', true, 'right'); ?></title>
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
   <script src="<?php echo get_template_directory_uri(); ?>/assets/js/vendor/modernizr-2.6.2.min.js"></script>
