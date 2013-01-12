bd76ea6519df46c0d02ad8ba437ae2f632f59b0c
Nitin Hayaran <nitinhayaran@gmail.com>
2013-01-12 22:54:59 +0530

    Added Author vCard

--- templates/entry-meta-single.php
+++ templates/entry-meta-single.php
@@ -1,3 +1,6 @@
+<address class="vcard">
+	<i class="icon-user"></i> Author: <a class="fn url" href="http://www.nitinh.com/about/">Nitin Hayaran</a>
+</address>
 <time class="updated" datetime="<?php echo get_the_time('c'); ?>" pubdate><i class="icon-time"></i> <?php echo sprintf(__('%s', 'roots'), get_the_date()); ?></time>
 <?php the_tags('<div class="entry-tags"><i class="icon-tags"></i> ',', ','</div>'); ?>
 <div class="entry-categories"><i class="icon-bookmark"></i> <?php echo get_the_category_list(', '); ?></div>
